bb51404548452926c7e437fd1930e4a0e4a93a82
flashrifle <flashrifle@gmail.com>
2024-08-13 16:58:43 +0900

    STD: ID Param을 이용해 하나의 POST 가져오기

--- nestjs_server/src/posts/posts.controller.ts
+++ nestjs_server/src/posts/posts.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { PostsService } from './posts.service';
 
 interface PostModel {
@@ -49,6 +49,10 @@ export class PostsController {
 
   // 2) GET /posts/:id
   // id에 해당하는 게시물을 가져온다
+  @Get(':id')
+  getPost(@Param('id') id: string) {
+    return posts.find((post) => post.id === +id);
+  }
 
   // 3) POST /posts 게시물을 변경한다
 
