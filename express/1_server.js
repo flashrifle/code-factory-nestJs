7cc4f94838797450518a9913d0c026feebfae95e
flashrifle <flashrifle@gmail.com>
2024-08-13 15:09:48 +0900

    STD: Path별로 다른응답 반환

--- express/1_server.js
+++ express/1_server.js
@@ -1,9 +1,26 @@
 const http = require('http');
+const url = require('url');
 
 const host = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+    const path = url.parse(req.url).pathname;
+
+    if(path === '/') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('<h1>Home Page</h1>');
+    } else if (path === '/post') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('<h1>Post Page</h1>');
+    } else if (path === '/user') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('<h1>User Page</h1>');
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.end('<h1>404 Page</h1>');
+    }
+
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('<h1>Hello world</h1>')
 });
