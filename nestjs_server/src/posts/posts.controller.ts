991a80ea8873eb373ac3b6f7522c37d13ffaba36
flashrifle <flashrifle@gmail.com>
2024-08-13 16:53:34 +0900

    STD: Get Posts 구현

--- nestjs_server/src/posts/posts.controller.ts
+++ nestjs_server/src/posts/posts.controller.ts
@@ -1,7 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { PostsService } from './posts.service';
 
-interface Post {
+interface PostModel {
+  id: number;
   author: string;
   title: string;
   content: string;
@@ -9,18 +10,49 @@ interface Post {
   commentCount: number;
 }
 
+let posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'author_test',
+    title: 'title_test',
+    content: 'title_test',
+    likeCount: 0,
+    commentCount: 0,
+  },
+  {
+    id: 2,
+    author: 'author_test2',
+    title: 'title_test2',
+    content: 'title_test2',
+    likeCount: 1,
+    commentCount: 1,
+  },
+  {
+    id: 3,
+    author: 'author_test3',
+    title: 'title_test3',
+    content: 'title_test3',
+    likeCount: 3,
+    commentCount: 3,
+  },
+];
+
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
+  // 1) GET /posts 모든 게시물을 가져온다
   @Get()
-  getPost(): Post {
-    return {
-      author: 'author_test',
-      title: 'title_test',
-      content: 'title_test',
-      likeCount: 0,
-      commentCount: 0,
-    };
+  getPosts() {
+    return posts;
   }
+
+  // 2) GET /posts/:id
+  // id에 해당하는 게시물을 가져온다
+
+  // 3) POST /posts 게시물을 변경한다
+
+  // 4) PUT /posts/:id id에 해당하는 개시물을 변경한다
+
+  // 5) DELETE /posts/:id id에 해당하는 개시물을 삭제
 }
