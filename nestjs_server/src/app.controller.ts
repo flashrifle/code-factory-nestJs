3efbb757d94dfe5629393cd3a06ab9c072d87f43
flashrifle <flashrifle@gmail.com>
2024-08-13 15:47:46 +0900

    STD: Post JSON 반환

--- nestjs_server/src/app.controller.ts
+++ nestjs_server/src/app.controller.ts
@@ -1,22 +1,34 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+/*
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+ */
+
+interface Post {
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHome() {
-    return 'Home Page';
-  }
-
-  @Get('post')
-  getPost() {
-    return 'Post Page';
-  }
-
-  @Get('user')
-  getUser() {
-    return 'User Page';
+  getPost(): Post {
+    return {
+      author: 'author_test',
+      title: 'title_test',
+      content: 'title_test',
+      likeCount: 0,
+      commentCount: 0,
+    };
   }
 }
