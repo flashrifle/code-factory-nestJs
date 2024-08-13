5bfa2ca0e95043a91c6f634a2997744cdca92f02
flashrifle <flashrifle@gmail.com>
2024-08-13 16:41:33 +0900

    STD: Controller, Path 모듈 생성

--- nestjs_server/src/app.controller.ts
+++ nestjs_server/src/app.controller.ts
@@ -1,34 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-/*
-  author: string;
-  title: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
- */
-
-interface Post {
-  author: string;
-  title: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
-}
-
-@Controller('post')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get('post')
-  getPost(): Post {
-    return {
-      author: 'author_test',
-      title: 'title_test',
-      content: 'title_test',
-      likeCount: 0,
-      commentCount: 0,
-    };
-  }
 }
