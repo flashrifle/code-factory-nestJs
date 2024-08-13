7b93b6670618c7014cfab091b85b90ecc7c013f7
flashrifle <flashrifle@gmail.com>
2024-08-13 17:01:51 +0900

    STD: Not Found Exception

--- nestjs_server/src/posts/posts.controller.ts
+++ nestjs_server/src/posts/posts.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
 import { PostsService } from './posts.service';
 
 interface PostModel {
@@ -51,7 +51,13 @@ export class PostsController {
   // id에 해당하는 게시물을 가져온다
   @Get(':id')
   getPost(@Param('id') id: string) {
-    return posts.find((post) => post.id === +id);
+    const post = posts.find((post) => post.id === +id);
+
+    if (!post) {
+      throw new NotFoundException();
+    } else {
+      return post;
+    }
   }
 
   // 3) POST /posts 게시물을 변경한다
