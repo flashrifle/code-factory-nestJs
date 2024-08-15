0f30a3e74c7c19a6585e5e2b4991d8bb366c6eea
flashrifle <flashrifle@gmail.com>
2024-08-15 15:51:18 +0900

    STD: Column Property 정리

--- type_orm/src/entity/user.entity.ts
+++ type_orm/src/entity/user.entity.ts
@@ -27,7 +27,30 @@ export class UserModel {
   id: number;
 
   // 제목
-  @Column()
+  @Column({
+    // 데이터베이스에서 인지하는 칼럼 타입
+    // 자동 유추, 특정 값이 필요하면 넣는다
+    type: 'varchar',
+    // 데이터베이스 칼럼 이름
+    // 프로퍼티 이름으로 자동 유추
+    name: 'title',
+    // 값의 길이
+    // 실제 입력 할 수 있는 글자의 길이가 300
+    length: 300,
+    // null 이 가능한지
+    nullable: true,
+    // true면 처음 저장할 때만 값 지정 가능
+    // 이후엔 불가능
+    update: true,
+    // find()를 실행할 때 기본으로 값을 불러올지
+    // 기본값이 true
+    select: false,
+    // 기본 값
+    // 아무것도 입력 안했을 때 들어가는 값
+    default: 'default value',
+    // 칼럼 중 유일무이한 값이 돼야 하는지 ex) 이메일 ..
+    unique: true,
+  })
   title: string;
 
   // 데이터 생성일자
