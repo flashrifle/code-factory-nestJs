516fc3e9ee59a6ab1b822e9228f9fa7c6f884a50
flashrifle <flashrifle@gmail.com>
2024-08-18 09:51:31 +0900

    STD: 토큰 재발급 로직

--- nestjs_server/src/auth/auth.controller.ts
+++ nestjs_server/src/auth/auth.controller.ts
@@ -5,8 +5,30 @@ import { AuthService } from './auth.service';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Post('token/access')
+  postTokenAccess(@Headers('authorization') rawToken: string) {
+    const token = this.authService.extractTokenFromHeader(rawToken, true);
+
+    const newToken = this.authService.rotateToken(token, false);
+
+    return {
+      accessToken: newToken,
+    };
+  }
+
+  @Post('token/refresh')
+  postTokenRefresh(@Headers('authorization') rawToken: string) {
+    const token = this.authService.extractTokenFromHeader(rawToken, true);
+
+    const newToken = this.authService.rotateToken(token, true);
+
+    return {
+      accessToken: newToken,
+    };
+  }
+
   @Post('login/email')
-  loginEmail(@Headers('authorization') rawToken: string) {
+  postLoginEmail(@Headers('authorization') rawToken: string) {
     // email:password -> Base64
     const token = this.authService.extractTokenFromHeader(rawToken, false);
     const credentials = this.authService.decodeBasicToken(token);
@@ -15,7 +37,11 @@ export class AuthController {
   }
 
   @Post('register/email')
-  registerEmail(@Body('nickname') nickname: string, @Body('email') email: string, @Body('password') password: string) {
+  postRegisterEmail(
+    @Body('nickname') nickname: string,
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ) {
     return this.authService.registerWithEmail({
       nickname,
       email,
