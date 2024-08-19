a3c9bba735a912e8690a8bc1aef6106313d07262
flashrifle <flashrifle@gmail.com>
2024-08-19 21:32:01 +0900

    STD: BasicTokenGuard 구현

--- nestjs_server/src/auth/auth.controller.ts
+++ nestjs_server/src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post, Headers } from '@nestjs/common';
+import { Body, Controller, Post, Headers, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { MaxLengthPipe, MinLengthPipe, PasswordPipe } from './pipe/password.pipe';
+import { BasicTokenGuard } from './guard/basic-token';
 
 @Controller('auth')
 export class AuthController {
@@ -29,6 +30,7 @@ export class AuthController {
   }
 
   @Post('login/email')
+  @UseGuards(BasicTokenGuard)
   postLoginEmail(@Headers('authorization') rawToken: string) {
     // email:password -> Base64
     const token = this.authService.extractTokenFromHeader(rawToken, false);
