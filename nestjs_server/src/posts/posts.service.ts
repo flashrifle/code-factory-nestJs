2ce3670876c3ed86746650ba55f870bdfa7c04b0
flashrifle <flashrifle@gmail.com>
2024-08-22 14:51:07 +0900

    STD: CursorPagination 메타데이터 생성

--- nestjs_server/src/posts/posts.service.ts
+++ nestjs_server/src/posts/posts.service.ts
@@ -5,6 +5,7 @@ import { PostsModel } from './entities/post.entity';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 import { PaginatePostDto } from './dto/paginate-post.dto';
+import { HOST, PROTOCOL } from '../common/const/env.const';
 
 export interface PostModel {
   id: number;
@@ -50,6 +51,31 @@ export class PostsService {
       },
       take: dto.take,
     });
+
+    // 해당되는 포스트가 0개 이상이면
+    // 마지막 포스트를 가져오고
+    // 아니면 null을 반환
+    const lastItem = posts.length > 0 ? posts[posts.length - 1] : null;
+
+    const nextUrl = lastItem && new URL(`${PROTOCOL}://${HOST}/posts`);
+    if (nextUrl) {
+      /*
+      DTO 의 키값들을 루핑하면서
+      키값에 해당되는 벨류가 존재하면
+      param에 그대로 붙여넣는다.
+
+      단 where__id_more_than 값만 lastItem의 마지막 값으로 넣어준다.
+       */
+      for (const key of Object.keys(dto)) {
+        if (dto[key]) {
+          if (key !== 'where__id_more_than') {
+            nextUrl.searchParams.append(key, dto[key]);
+          }
+        }
+      }
+
+      nextUrl.searchParams.append('where__id_more_than', lastItem.id.toString());
+    }
     /*
     Response
 
@@ -60,7 +86,14 @@ export class PostsService {
     count: 응답한 데이터의 갯수
     next: 다음 요청을 할 때 사용할 URL
      */
-    return { data: posts };
+    return {
+      data: posts,
+      cursor: {
+        after: lastItem?.id,
+      },
+      count: posts.length,
+      next: nextUrl?.toString(),
+    };
   }
 
   async getPostById(id: number) {
