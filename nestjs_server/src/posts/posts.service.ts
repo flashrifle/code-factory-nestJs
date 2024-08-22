1a0a73195c3be2b0a3919338d7e7ed1884956f5f
flashrifle <flashrifle@gmail.com>
2024-08-22 15:34:27 +0900

    STD: 다음 커서가 존재하지 않을때 undefined 대신 null 반환

--- nestjs_server/src/posts/posts.service.ts
+++ nestjs_server/src/posts/posts.service.ts
@@ -89,7 +89,7 @@ export class PostsService {
     return {
       data: posts,
       cursor: {
-        after: lastItem?.id,
+        after: lastItem?.id ?? null,
       },
       count: posts.length,
       next: nextUrl?.toString(),
