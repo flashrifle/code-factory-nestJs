cdaff08cef2d32fb8bd2f0b58c65373c9f3aa118
flashrifle <flashrifle@gmail.com>
2024-08-22 16:04:01 +0900

    STD: 응답에 total 프로퍼티 추가

--- nestjs_server/src/posts/posts.service.ts
+++ nestjs_server/src/posts/posts.service.ts
@@ -55,7 +55,7 @@ export class PostsService {
 
     [1],[2],[3],[4]
      */
-    const posts = await this.postsRepository.find({
+    const [posts, count] = await this.postsRepository.findAndCount({
       skip: dto.take * (dto.page - 1),
       take: dto.take,
       order: {
@@ -65,6 +65,7 @@ export class PostsService {
 
     return {
       data: posts,
+      total: count,
     };
   }
 
