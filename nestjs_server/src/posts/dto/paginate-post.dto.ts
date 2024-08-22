c944386706467068250551e64d3a255cf650df92
flashrifle <flashrifle@gmail.com>
2024-08-22 15:41:32 +0900

    STD: 내림차순 NEXT 토큰 로직

--- nestjs_server/src/posts/dto/paginate-post.dto.ts
+++ nestjs_server/src/posts/dto/paginate-post.dto.ts
@@ -2,17 +2,22 @@ import { IsIn, IsNumber, IsOptional } from 'class-validator';
 import { Type } from 'class-transformer';
 
 export class PaginatePostDto {
+  @IsNumber()
+  @IsOptional()
+  where__id_less_than?: number;
   // 이전 마지막 데이터의 ID
   // 이 프로퍼티에 입력된 ID 보다 높은 ID 부터 값을 가져오기
   @IsNumber()
   @IsOptional()
   where__id_more_than?: number;
 
+  // 10, 9, 8, 7
+
   // 정렬
   // createdAt -> 생성된 시간의 내림차/오름차 설정
-  @IsIn(['ASC'])
+  @IsIn(['ASC', 'DESC'])
   @IsOptional()
-  order__createdAt: 'ASC' = 'ASC';
+  order__createdAt: 'ASC' | 'DESC' = 'ASC';
 
   // 몇개의 데이터를 가져올지
   @IsNumber()
