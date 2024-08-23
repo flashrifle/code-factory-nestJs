1daee7fa205050f111d48d41bc6ddba813c09a63
flashrifle <flashrifle@gmail.com>
2024-08-23 14:10:39 +0900

    STD: BasePaginationDto 생성

---
+++ nestjs_server/src/common/dto/base-pagination.dto.ts
@@ -0,0 +1,29 @@
+import { IsIn, IsNumber, IsOptional } from 'class-validator';
+
+export class BasePaginationDto {
+  @IsNumber()
+  @IsOptional()
+  page?: number;
+
+  @IsNumber()
+  @IsOptional()
+  where__id_less_than?: number;
+  // 이전 마지막 데이터의 ID
+  // 이 프로퍼티에 입력된 ID 보다 높은 ID 부터 값을 가져오기
+  @IsNumber()
+  @IsOptional()
+  where__id_more_than?: number;
+
+  // 10, 9, 8, 7
+
+  // 정렬
+  // createdAt -> 생성된 시간의 내림차/오름차 설정
+  @IsIn(['ASC', 'DESC'])
+  @IsOptional()
+  order__createdAt: 'ASC' | 'DESC' = 'ASC';
+
+  // 몇개의 데이터를 가져올지
+  @IsNumber()
+  @IsOptional()
+  take: number = 20;
+}
