b3774e5af4509f43fed10ece3178ab5517c48c46
flashrifle <flashrifle@gmail.com>
2024-08-25 08:40:05 +0900

    STD: DTO 프로퍼티 whitelisting 하기

--- nestjs_server/src/main.ts
+++ nestjs_server/src/main.ts
@@ -11,6 +11,10 @@ async function bootstrap() {
       transformOptions: {
         enableImplicitConversion: true,
       },
+      // DTO 에 적용되지 않은 프로퍼티 삭제
+      whitelist: true,
+      // 적용되지 않은 프로퍼티가 들어가면 에러발생
+      forbidNonWhitelisted: true,
     }),
   );
 
