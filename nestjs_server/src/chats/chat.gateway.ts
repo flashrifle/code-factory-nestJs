c31fa643897af90386eee89c5a25a73bfaa98bb5
flashrifle <flashrifle@gmail.com>
2024-08-30 12:08:54 +0900

    STD: Enter Chat 이벤트 업데이터 & WSException 던지기

--- nestjs_server/src/chats/chat.gateway.ts
+++ nestjs_server/src/chats/chat.gateway.ts
@@ -5,10 +5,12 @@ import {
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
+  WsException,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { CreateChatDto } from './dto/create-chat.dto';
 import { ChatsService } from './chats.service';
+import { EnterChatDto } from './dto/enter-chat.dto';
 
 @WebSocketGateway({
   // ws://localhost:3000/chats
@@ -30,15 +32,22 @@ export class ChatGateway implements OnGatewayConnection {
   }
 
   @SubscribeMessage('enter_chat')
-  enterChat(
+  async enterChat(
     // 방의 chat ID를 리스트로 받는다.
-    @MessageBody() data: number[],
+    @MessageBody() data: EnterChatDto,
     @ConnectedSocket() socket: Socket,
   ) {
-    for (const chatId of data) {
-      // socket.join()
-      socket.join(chatId.toString());
+    for (const chatId of data.chatIds) {
+      const exists = await this.chatsService.checkIfChatExists(chatId);
+      if (!exists) {
+        throw new WsException({
+          code: 100,
+          message: `존재하지 않는 chat 입니다. chatId: ${chatId}`,
+        });
+      }
     }
+
+    socket.join(data.chatIds.map((x) => x.toString()));
   }
 
   // socket.on('send_msg', (msg) => {console.log(msg)};
